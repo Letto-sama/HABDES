e79cddf8e2f3101e56ad6822a2a889305a072bab
Letto <rodrigo0830@gmail.com>
2020-10-18 03:16:34 -0500

    Pequeñas optimizaciones

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const web = require(".")
-
-app.get("/", (req, res) =>res.send("./index.html"));
-
-
-app.listen(3000);
-console.log("Server on port", 3000)
